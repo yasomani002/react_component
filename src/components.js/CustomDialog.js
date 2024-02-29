455d05babbeae39b1963f4e939424999ae41dca8
Yash Somani <yashsomani.capermint@gmail.com>
2024-02-29 18:05:46 +0530

    [WiP] custome form

--- src/components.js/CustomDialog.js
+++ src/components.js/CustomDialog.js
@@ -2,16 +2,25 @@ import React, { useState } from 'react'
 import { Dialog } from "@mui/material";
 import { makeStyles } from '@mui/styles';
 import Button from './Button';
+import CustomForm from './CustomForm';
 
 
 const useStyles = makeStyles({
     root: {
-        height: '400px',
+        minHeight: '400px',
+        height: 'auto',
         width: '500px',
         padding: '10px'
     },
+    dialog__header: {
+        height: '10%',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
     dialog__body: {
-        height: '85%',
+        height: '75%',
         width: '100%',
     },
     dialog__footer: {
@@ -28,20 +37,63 @@ const useStyles = makeStyles({
 
 function CustomDialog() {
     const classes = useStyles()
-    const [ openDialog, setOpenDialog] = useState(true)
+    const [openDialog, setOpenDialog] = useState(true)
+    const [formData, setFormData] = useState({});
     const handleClose = () => {
         setOpenDialog(false)
     }
+    const handleSubmitButtonClick = () => {
+        // Call the submitForm function from CustomForm
+        customFormRef.current.submitForm();
+    };
+
+    const handleFormSubmit = (data) => {
+        console.log('Form data received in parent:', data);
+        setFormData(data);
+    };
+
+    const customFormRef = React.createRef();
+
+    const idData = { foName: '', LoName: '', eOMail: '', pNumber: '' }
+    const fieldData = [
+        {
+            id: 'foName',
+            label: 'First Name:',
+        },
+        {
+            id: 'LoName',
+            label: 'Last Name:',
+        },
+        {
+            id: 'eOMail',
+            label: 'Email:',
+        },
+        {
+            id: 'pNumber',
+            label: 'phone number:',
+        },
+    ];
     return (
         <Dialog open={openDialog}>
             <div className={classes.root}>
-
-                <div className={classes.dialog__body}></div>
+                <div className={classes.dialog__header}>
+                    <h2>Add Task Form</h2>
+                </div>
+                <div className={classes.dialog__body}>
+                    <CustomForm
+                        ref={customFormRef}
+                        onSubmit={handleFormSubmit}
+                        fieldData={fieldData}
+                        idData={idData}
+                    />
+                </div>
                 <div className={classes.dialog__footer}>
-                    <Button 
-                    onClick={handleClose}
+                    <Button
+                        onClick={handleClose}
                     >Close</Button>
-                    <Button>Save</Button>
+                    <Button
+                        onClick={handleSubmitButtonClick}
+                    >Save</Button>
                 </div>
             </div>
         </Dialog>
