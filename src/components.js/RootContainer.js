dbad49cbb8018dea91169f6ffa16250a0f401f22
Yash Somani <yashsomani.capermint@gmail.com>
2024-05-22 17:52:17 +0530

    [WD] design update | add loader | usedatafetch

--- src/components.js/RootContainer.js
+++ src/components.js/RootContainer.js
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@mui/styles'
-import axios from 'axios'
 import Typo from './Typo'
+import useDataFetch from './custom-hooks/useDataFetch'
+import Loader from './Loader'
 
 
 const useStyles = makeStyles({
     root: {
         display: 'flex',
         flexWrap: 'wrap',
+        alignItems: 'center',
         gap: '1rem',
         justifyContent: 'center',
-        marginTop: '1rem',
+        backgroundColor: '#000000',
+        minHeight: '100vh',
+        padding: '1rem',
     },
     container: {
-        width: '430px',
-        height: '240px',
-        border: '2px solid #000000',
+        width: '400px',
+        height: '200px',
         borderRadius: '1rem',
         transition: 'transform 0.2s',
         display: 'flex',
@@ -23,14 +26,39 @@ const useStyles = makeStyles({
         alignItems: 'center',
         justifyContent: 'center',
         cursor: 'pointer',
+        backgroundColor: '#ffffff',
+        boxShadow: '#696c80 6px 6px 7px',
         '&:hover': {
             transform: 'rotate(-1.5deg) scale(1.03)'
         },
+    },
+    '@media (max-width: 900px)': {
+        container: {
+            width: '300px',
+            height: '150px',
+        }
+    },
+    '@media (max-width: 600px)': {
+        container: {
+            width: '160px',
+            height: '80px',
+        }
     }
 })
 
 function RootContainer() {
     const classes = useStyles()
+    const [leftResponse, setLeftResponse] = useState()
+
+    // call api
+    const { data, loading } = useDataFetch('https://65b3f716770d43aba47ac437.mockapi.io/portfolio/getCommanComponent')
+    useEffect(() => {
+        if (data) {
+            setLeftResponse(data.data)
+        }
+    }, [data])
+
+    // copy code on click
     const handleClick = (value) => {
         navigator.clipboard.writeText(value).then(() => {
             alert('Content copied to clipboard!');
@@ -38,25 +66,24 @@ function RootContainer() {
             console.error('Could not copy text: ', err);
         });
     };
-    const [error, setError] = useState()
-    const [leftResponse, setLeftResponse] = useState()
-    useEffect(() => {
-        axios.get("https://65b3f716770d43aba47ac437.mockapi.io/portfolio/getCommanComponent")
-            .then((response) => setLeftResponse(response?.data))
-            .catch((error) => setError(error))
-    }, [])
     return (
-        <div className={classes.root}>
+        <>
+            {loading ?
+                <Loader />
+                :
+                <div className={classes.root}>
 
-            {leftResponse?.map((item, index) => {
-                return (
-                    <div key={index} className={classes.container} onClick={() => handleClick(item.contentToCopy)}>
-                        <Typo variant='ka01'>{item.title}</Typo>
-                        <Typo variant='lb02'>Click to copy code.</Typo>
-                    </div>
-                )
-            })}
-        </div>
+                    {leftResponse?.map((item, index) => {
+                        return (
+                            <div key={index} className={classes.container} onClick={() => handleClick(item.contentToCopy)}>
+                                <Typo variant='ka01'>{item.title}</Typo>
+                                <Typo variant='lb02'>Click to copy code.</Typo>
+                            </div>
+                        )
+                    })}
+                </div>
+            }
+        </>
     )
 }
 
